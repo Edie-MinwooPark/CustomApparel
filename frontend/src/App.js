76e29e5ed7cc4d95a7cc22b15ebdc08b70a58f26
ojhhh <winnel@naver.com>
2023-08-03 20:14:49 +0900

    layout add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Main from "./page/MainPage/Main";
 
 function App() {
   return (
     <div className="App">
-    
+      <Main />
     </div>
   );
 }
