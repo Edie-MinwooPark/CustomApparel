d32469905efe23099bf5b253c44005fb358a7e65
gusdnr205 <gusdnr205@gmail.com>
2023-08-04 10:53:16 +0900

    프론트 라우터 구성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import Main from "./page/MainPage/Main";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route, Routes, BrowserRouter as Router, Outlet } from 'react-router-dom';
+import { Custom, Photo, Signup, Login, Nav } from "../src/page/index"
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Main />
-      </BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/custom' element={<Custom />} />
+        
+        <Route path='/photo' element={<Photo />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/nav' element={<Nav />} />
+
+      </Routes>
+      
 
     </div>
   );
--- frontend/src/page/MainPage/Main.jsx
+++ frontend/src/page/MainPage/Main.jsx
@@ -1,23 +1,14 @@
 import React from "react";
 import { MainWrap } from "../MainPage/Main.styled";
-import { Route, Routes, BrowserRouter as Router, Outlet } from 'react-router-dom';
-import { Custom, Photo, Signup, Login,Nav} from "../index"
+import Nav from "../NavPage/Nav";
 
 const Main = () => {
   return (
     <div>
-      <Routes>
-        <Route path='/custom' element={<Custom />} />
-        <Route path='/photo' element={<Photo />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/nav' element={<Nav />} />
-  
-      </Routes>
-      <Nav/>
-            <MainWrap>
-          <div className="mainContainer"></div>
-        </MainWrap>
+      <Nav />
+      <MainWrap>
+        <div className="mainContainer"></div>
+      </MainWrap>
 
     </div>
   );
