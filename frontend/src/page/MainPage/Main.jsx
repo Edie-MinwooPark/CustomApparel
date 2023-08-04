91c8db61f578c78d1067468895474344e125359a
ojhhh <winnel@naver.com>
2023-08-04 10:53:08 +0900

    img test

--- frontend/src/page/MainPage/Main.jsx
+++ frontend/src/page/MainPage/Main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Nav from "../NavPage/Nav";
 import { MainWrap } from "../MainPage/Main.styled";
+import TestBackgroundImg from "./testimg.png";
 
 const Main = () => {
   return (
     <div>
       <Nav />
       <MainWrap>
-        <div className="mainContainer"></div>
+        <div className="mainContainer">
+          <img src={TestBackgroundImg} alt="" />
+        </div>
       </MainWrap>
     </div>
   );
--- frontend/src/page/MainPage/Main.styled.jsx
+++ frontend/src/page/MainPage/Main.styled.jsx
@@ -8,4 +8,8 @@ export const MainWrap = styled.div`
     border: 1px solid;
     background-color: black;
   }
+  & .mainContainer img {
+    width: 100%;
+    height: 100%;
+  }
 `;
