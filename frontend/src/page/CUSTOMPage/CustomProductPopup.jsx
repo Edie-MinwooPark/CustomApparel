b3d4a1c58f7f00b0f6939746a8e0c3ea8a4b6625
ojhhh <winnel@naver.com>
2023-08-09 11:31:58 +0900

    202308091131 merge to edie before

--- frontend/src/page/CUSTOMPage/CustomProductPopup.jsx
+++ frontend/src/page/CUSTOMPage/CustomProductPopup.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { PopupWrap, Closebtn } from "./CustomProductPopup.styled";
+import { useSelector } from "react-redux";
 
 const CustomProductPopup = ({ data }) => {
+  // customSlice의 초기값을 가져옴
+  const shirtInfo = useSelector((state) => state.custom.basic);
+
+  function handleChangeProduct(e) {
+    console.log(e.target);
+  }
   return (
     <>
       <PopupWrap>
@@ -16,29 +23,28 @@ const CustomProductPopup = ({ data }) => {
           </div>
           <div className="popupBody">
             {/*  */}
-            <div className="popupCard">
+            <div className="popupCard" onClick={handleChangeProduct}>
               <div className="cardImg"></div>
               <div className="cardText">
                 <span>민우의 개쩌는 티셔츠 2</span>
                 <span>100,000,000KRW</span>
               </div>
             </div>
-            {/*  */}
-            <div className="popupCard">
+
+            {/* <div className="popupCard">
               <div className="cardImg"></div>
               <div className="cardText">
                 <span>민우의 개쩌는 티셔츠 2</span>
                 <span>100,000,000KRW</span>
               </div>
             </div>
-            {/*  */}
             <div className="popupCard">
               <div className="cardImg"></div>
               <div className="cardText">
                 <span>민우의 개쩌는 티셔츠 2</span>
                 <span>100,000,000KRW</span>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </PopupWrap>
