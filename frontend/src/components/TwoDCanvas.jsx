d8329159709a2bf466c048b7bb733f1c164cf4f1
Edie-MinwooPark <alsdn8055@gmail.com>
2023-08-14 13:20:56 +0900

    2D 이미지 넣는 중

--- frontend/src/components/TwoDCanvas.jsx
+++ frontend/src/components/TwoDCanvas.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { useState,useEffect } from 'react';
 
 const TwoDCanvas = () => {
-  const cloth = useSelector(state=>state.cloth.clothType)
+  const clothType = useSelector(state=>state.cloth.clothType);
+  const [canvas,setCanvas] = useState(null);
+
+  useEffect(()=>{
+    const canvasElement = document.getElementById('canvas');
+    setCanvas(canvasElement);
+
+    const ctx = canvasElement.getContext('2d');
+    const backgroundImage = new Image();
+    backgroundImage.src = "https://image.toast.com/aaaaab/ticketlink/TKL_10/BigBanner_mo_%EC%82%BC%EC%B4%9D%EC%82%AC.jpg"
+
+    backgroundImage.onload = ()=>{
+      console.log("?")
+      ctx.drawImage(backgroundImage,0,0)
+    }
+
+  },[])
+
+
+
   return (
-    <div>{cloth}</div>
+    <canvas id='canvas' width='1000' height='500' ></canvas>
   )
 }
 
