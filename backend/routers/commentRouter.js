3cfd1b2564d24524120b7e6c9362fa1c432b8726
youdonghee <0103donghee@naver.com>
2023-08-21 17:54:40 +0900

    recomment fin

--- backend/routers/commentRouter.js
+++ backend/routers/commentRouter.js
@@ -1,15 +1,17 @@
 const router = require("express").Router();
 const {
   comment,
-  recoment,
+  reComment,
   getComments,
+  getRecomments,
 } = require("../controller/commnetsController");
 
 // 댓글 보여주기
 router.get("/comments/:postId", getComments);
+router.get("/recomments/:commentId", getRecomments);
 
 // // 대댓글
-router.post("/recomments", recoment);
+router.post("/recomments", reComment);
 // // 댓글 추가 라우트
 // :id 이거 있으면 무조건 아래에 두기(중간에 가로채서 밑에거 실행안됨)
 router.post("/comments", comment);
--- frontend/src/page/POSTDETAILpage/PostDetail.jsx
+++ frontend/src/page/POSTDETAILpage/PostDetail.jsx
@@ -10,8 +10,8 @@ import {
   Viewdiv,
   Text,
   Text_Span,
-  More,
   Comment_Box,
+  More,
 } from "./PostDetail.styled";
 import axios from "axios";
 import { LikeIcon } from "../../components/layout/userbox";
@@ -21,8 +21,7 @@ import { useSelector } from "react-redux";
 
 const PROXY = process.env.REACT_APP_PROXY;
 
-const PostDetail = (post) => {
-  // 게시글 가져오기
+const PostDetail = () => {
   const fetchPost = async (postId) => {
     const { data } = await axios.get(`${PROXY}/post/detail/${postId}`, {
       params: { id: postId },
@@ -32,15 +31,20 @@ const PostDetail = (post) => {
     return data;
   };
 
-  // 댓글가져오기
-  const [commentsList, setCommentsList] = useState();
   const fetchComments = async (postId) => {
     const { data } = await axios.get(`${PROXY}/comment/comments/${postId}`);
     return data.comments;
   };
+  const fetchReComments = async (commentId) => {
+    const { data } = await axios.get(
+      `${PROXY}/comment/recomments/${commentId}`
+    );
+    return data.recomments;
+  };
 
-  const [likes, setLikes] = useState(post.likes || 0);
-  const [addComments, setAddComments] = useState(post.comments); // 댓글
+  const [likes, setLikes] = useState(0);
+  const [addComments, setAddComments] = useState(""); // 댓글
+  const [recommentInput, setRecommentInput] = useState({}); // 대댓글
 
   const [isLiked, setIsLiked] = useState(false); // 좋아요 상태를 추적하기 위한 state
   const [expanded, setExpanded] = useState(false);
@@ -75,25 +79,35 @@ const PostDetail = (post) => {
       }
 
       let likesData = JSON.parse(postdata.likes);
-      const likeUser = likesData.find((value) => value == user_info.id);
+      let updatedLikesData;
+      const likeUserIndex = likesData.findIndex(
+        (value) => value === user_info.id
+      );
+
+      if (likeUserIndex !== -1) {
+        // 이미 좋아요한 경우, 좋아요 취소 처리
+        updatedLikesData = likesData.filter((value) => value !== user_info.id);
+      } else {
+        // 좋아요하지 않은 경우, 좋아요 추가 처리
+        updatedLikesData = [...likesData, user_info.id];
+      }
 
       let url = `${PROXY}/post/postLikes/`;
-      let action = likeUser ? "unlike" : "like"; // like 또는 unlike를 결정
+      let action = likeUserIndex !== -1 ? "unlike" : "like";
 
       const response = await axios.post(url, {
-        action: action, // 서버에서 이 action을 확인하여 좋아요 추가 또는 취소 처리
+        action: action,
         user_id: user_info.id,
         post_id: postId,
         post_title: postdata.post_title,
         post_content: postdata.post_content,
+        likes: JSON.stringify(updatedLikesData),
       });
 
       if (response.data.success) {
         console.log("여기까진 오니?");
-        setIsLiked((prevState) => !prevState);
-        setLikes((prevLikes, prevState) =>
-          prevState ? prevLikes - 1 : prevLikes + 1
-        );
+        setIsLiked(!isLiked); // 토글 상태 변경
+        setLikes(updatedLikesData.length); // 좋아요 수 업데이트
         refetch();
       }
     } catch (error) {
@@ -101,7 +115,7 @@ const PostDetail = (post) => {
     }
   };
 
-  // 댓글 추가 기능
+  // 댓글추가
   const handleCommentSubmit = async () => {
     try {
       const response = await axios.post(`${PROXY}/comment/comments/`, {
@@ -110,21 +124,69 @@ const PostDetail = (post) => {
         addComments,
         postId,
       });
-      console.log("succccesssss?", response);
       if (response.data.success) {
+        console.log("succccesssss?", response);
         console.log("여기까지옴?");
         const updatedComments = await fetchComments(postId);
-        setCommentsList(updatedComments);
+
+        // 기존댓글 + 대댓글 + 새댓글 합침
+        const commetsWithRecomments = updatedComments.map((comment) => ({
+          ...comment,
+          RECOMMENTs:
+            commentsList.find((c) => c.id === comment.id)?.RECOMMENTs || [],
+        }));
+        setCommentsList(commetsWithRecomments);
+        setAddComments(""); // 댓글 입력 창 비우기
       }
     } catch (error) {
       console.log("댓글 추가 실패", error);
     }
   };
   // 댓글 목록을 저장하는 상태
+  const [commentsList, setCommentsList] = useState([]);
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
+  // 대댓글 입력창 토글하는 함수
+  const toggleRecommentInput = (commentId) => {
+    setRecommentInput((prevInput) => ({
+      ...prevInput,
+      [commentId]: !prevInput[commentId],
+    }));
+  };
+
+  // 대댓글 추가 함수
+  const handleRecommentSubmit = async (commentId) => {
+    try {
+      const response = await axios.post(`${PROXY}/comment/recomments/`, {
+        user_id: user_info.user_id,
+        profile_img: user_info.profile_img,
+        recomments: recommentInput[commentId],
+        recomment_id: commentId, // 댓글id 넣기
+        // postId,
+      });
+
+      if (response.data.success) {
+        console.log("commentId :", commentId);
+        const updatedComments = await fetchReComments(commentId);
+        setCommentsList((prevComments) =>
+          prevComments.map((comment) =>
+            comment.id === commentId
+              ? { ...comment, RECOMMENTs: updatedComments }
+              : comment
+          )
+        );
+        setRecommentInput((prevInput) => ({
+          ...prevInput,
+          [commentId]: "",
+        }));
+        toggleRecommentInput(commentId); // 대댓글 입력창 닫기
+      }
+    } catch (error) {
+      console.log("대댓글 추가 실패", error);
+    }
+  };
   return (
     <>
       <Nav />
@@ -141,10 +203,10 @@ const PostDetail = (post) => {
           </StyledLikeIconWrapper>
           <CountContainer>
             <div className="like_count">
-              좋아요 <strong>{postdata.likes.length}</strong>개
+              좋아요 <strong>{likes}</strong>개
             </div>
             <Viewdiv className="comment_count">
-              댓글 <strong>{commentsList?.length}</strong>개
+              댓글 <strong>{commentsList.length}</strong>개
             </Viewdiv>
           </CountContainer>
           <Text
@@ -166,14 +228,51 @@ const PostDetail = (post) => {
             )}
           </Text>
           <Comment_Box>
-            {commentsList &&
-              commentsList.map(
-                (value) => (
-                  <li className="comment_content">{value.comments_content}</li>
-                )
-                // console.log(value.comments_content)
-              )}
-            <input onChange={(e) => setAddComments(e.target.value)}></input>
+            {/* {commentsList.map((value, index) => (
+              <li key={index} className="comment_content">
+                {value.comments_content}
+              </li>
+            ))} */}
+
+            {commentsList && commentsList.length > 0 ? (
+              commentsList.map((comment) => (
+                <div key={comment.id}>
+                  <div className="comment_content">
+                    {comment.comments_content}
+                  </div>
+                  <button onClick={() => toggleRecommentInput(comment.id)}>
+                    대댓글 달기
+                  </button>
+                  {recommentInput[comment.id] && (
+                    <>
+                      <input
+                        onChange={(e) =>
+                          setRecommentInput({
+                            ...recommentInput,
+                            [comment.id]: e.target.value,
+                          })
+                        }
+                      />
+                      <button onClick={() => handleRecommentSubmit(comment.id)}>
+                        등록
+                      </button>
+                    </>
+                  )}
+                  {/* 대댓글 목록 출력 */}
+                  {console.log("대댓글 데이터 comment", comment.RECOMMENTs)}
+                  {comment.RECOMMENTs &&
+                    comment.RECOMMENTs.length > 0 &&
+                    comment.RECOMMENTs.map((recomment) => (
+                      <div key={recomment.id} className="recomment_content">
+                        {recomment.recomments}
+                      </div>
+                    ))}
+                </div>
+              ))
+            ) : (
+              <div>댓글이 없습니다.</div>
+            )}
+            <input onChange={(e) => setAddComments(e.target.value)} />
             <button onClick={handleCommentSubmit}>입력</button>
           </Comment_Box>
         </ContentBox>
