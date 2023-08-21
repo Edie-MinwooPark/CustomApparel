4d40957875bf4352956cf697b72a47e9044a5556
Edie-MinwooPark <alsdn8055@gmail.com>
2023-08-21 11:47:33 +0900

    추가된 decal 이동, 삭제 , 크기조절 기능 완성 // decal 전체삭제 기능 완성

--- frontend/src/components/KonvaCanvas.js
+++ frontend/src/components/KonvaCanvas.js
@@ -7,9 +7,10 @@ const KonvaCanvas = (props) => {
 
   
   const [selectedNodes, setSelectedNodes] = useState([]);
+  const [allDecals,setAllDecals] = useState([]);
   const [konvaStage, setKonvaStage] = useState(null);
   const [konvaLayer, setKonvaLayer] = useState(null);
-
+  const [konvaTr, setKonvaTr] = useState();
   const decalName = useSelector(state=>state.decal.decalName);
   const decalNum = useSelector(state=>state.decal.decalNum);
   // const decalNum = useSelector(state=>state.decal.decalNum);
@@ -30,29 +31,23 @@ const KonvaCanvas = (props) => {
       var layer = new Konva.Layer();
       stage.add(layer);
 
-      var rect1 = new Konva.Rect({
-        x: 60,
-        y: 60,
-        width: 100,
-        height: 90,
-        fill: 'red',
-        name: 'rect',
-        draggable: true,
-      });
-      layer.add(rect1);
-      var rect2 = new Konva.Rect({
-        x: 250,
-        y: 100,
-        width: 150,
-        height: 90,
-        fill: 'green',
-        name: 'rect',
-        draggable: true,
-      });
-      layer.add(rect2);
+
+      let image = new Image();
+      image.src = "kga.png";
+      let imageObj = new Konva.Image({
+        x: 50,
+        y: 50,
+        image: image,
+        width: 220,
+        height: 100,
+        draggable : true,
+      })
+      layer.add(imageObj);
+      setAllDecals([...allDecals, imageObj]);
 
       var tr = new Konva.Transformer();
       layer.add(tr);
+      setKonvaTr(tr);
 
       // by default select all shapes
       tr.nodes([]);
@@ -66,72 +61,74 @@ const KonvaCanvas = (props) => {
       selectionRectangleRef.current = selectionRectangle;
 
       var x1, y1, x2, y2;
-      stage.on('mousedown touchstart', (e) => {
-        // do nothing if we mousedown on any shape
-        if (e.target !== stage) {
-          return;
-        }
-        e.evt.preventDefault();
-        x1 = stage.getPointerPosition().x;
-        y1 = stage.getPointerPosition().y;
-        x2 = stage.getPointerPosition().x;
-        y2 = stage.getPointerPosition().y;
+      // stage.on('mousedown touchstart', (e) => {
+      //   // do nothing if we mousedown on any shape
+      //   if (e.target !== stage) {
+      //     return;
+      //   }
+      //   e.evt.preventDefault();
+      //   x1 = stage.getPointerPosition().x;
+      //   y1 = stage.getPointerPosition().y;
+      //   x2 = stage.getPointerPosition().x;
+      //   y2 = stage.getPointerPosition().y;
 
-        selectionRectangle.visible(true);
-        selectionRectangle.width(0);
-        selectionRectangle.height(0);
-      });
+      //   selectionRectangle.visible(true);
+      //   selectionRectangle.width(0);
+      //   selectionRectangle.height(0);
+      // });
 
-      stage.on('mousemove touchmove', (e) => {
-        // do nothing if we didn't start selection
-        if (!selectionRectangle.visible()) {
-          return;
-        }
+      // stage.on('mousemove touchmove', (e) => {
+      //   // do nothing if we didn't start selection
+      //   if (!selectionRectangle.visible()) {
+      //     return;
+      //   }
       
-        e.evt.preventDefault();
+      //   e.evt.preventDefault();
         
-        const containerRect = stage.container().getBoundingClientRect();
-        const mouseX = e.evt.clientX - containerRect.left;
-        const mouseY = e.evt.clientY - containerRect.top;
+      //   const containerRect = stage.container().getBoundingClientRect();
+      //   const mouseX = e.evt.clientX - containerRect.left;
+      //   const mouseY = e.evt.clientY - containerRect.top;
       
-        // Check if the mouse is outside the container bounds
-        if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) {
-          // Cancel the selection
-          selectionRectangle.visible(false);
-          return;
-        }
+      //   // Check if the mouse is outside the container bounds
+      //   if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) {
+      //     // Cancel the selection
+      //     selectionRectangle.visible(false);
+      //     return;
+      //   }
       
-        x2 = Math.max(Math.min(mouseX, width), 0);
-        y2 = Math.max(Math.min(mouseY, height), 0);
+      //   x2 = Math.max(Math.min(mouseX, width), 0);
+      //   y2 = Math.max(Math.min(mouseY, height), 0);
       
-        selectionRectangle.setAttrs({
-          x: Math.min(x1, x2),
-          y: Math.min(y1, y2),
-          width: Math.abs(x2 - x1),
-          height: Math.abs(y2 - y1),
-        });
-      });
+      //   selectionRectangle.setAttrs({
+      //     x: Math.min(x1, x2),
+      //     y: Math.min(y1, y2),
+      //     width: Math.abs(x2 - x1),
+      //     height: Math.abs(y2 - y1),
+      //   });
+      // });
       
 
-      stage.on('mouseup touchend', (e) => {
-        // do nothing if we didn't start selection
-        if (!selectionRectangle.visible()) {
-          return;
-        }
-        e.evt.preventDefault();
-        // update visibility in timeout, so we can check it in click event
-        setTimeout(() => {
-          selectionRectangle.visible(false);
-        });
 
-        var shapes = stage.find('.rect, .image, .text');
-        var box = selectionRectangle.getClientRect();
-        var selected = shapes.filter((shape) =>
-          Konva.Util.haveIntersection(box, shape.getClientRect())
-        );
-        tr.nodes(selected);
-        setSelectedNodes(selected)
-      });
+
+      // stage.on('mouseup touchend', (e) => {
+      //   // do nothing if we didn't start selection
+      //   if (!selectionRectangle.visible()) {
+      //     return;
+      //   }
+      //   e.evt.preventDefault();
+      //   // update visibility in timeout, so we can check it in click event
+      //   setTimeout(() => {
+      //     selectionRectangle.visible(false);
+      //   });
+
+      //   var shapes = stage.find('.rect, .image, .text');
+      //   var box = selectionRectangle.getClientRect();
+      //   var selected = shapes.filter((shape) =>
+      //     Konva.Util.haveIntersection(box, shape.getClientRect())
+      //   );
+      //   tr.nodes(selected);
+      //   setSelectedNodes(selected)
+      // });
 
       // clicks should select/deselect shapes
       stage.on('click tap', function (e) {
@@ -147,9 +144,9 @@ const KonvaCanvas = (props) => {
         }
 
         // do nothing if clicked NOT on our rectangles
-        if (!e.target.hasName('rect')) {
-          return;
-        }
+        // if (!e.target.hasName('rect')) {
+        //   return;
+        // }
 
         // do we pressed shift or ctrl?
         const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
@@ -179,11 +176,22 @@ const KonvaCanvas = (props) => {
 
     function destroySelected() {
       selectedNodes.forEach((node) => {
+        console.log(selectedNodes);
         node.destroy();
       });
   
       setSelectedNodes([]);
+      
       konvaLayer.batchDraw(); // Manually redraw the layer after making changes
+    }
+
+    function destroyAll() {
+      allDecals.forEach((node)=>{
+        node.destroy();
+      })
+      setAllDecals([]);
+
+      konvaLayer.batchDraw();
     }
 
     const [isClicked, setIsClicked] = useState(false);
@@ -206,9 +214,8 @@ const KonvaCanvas = (props) => {
     document.addEventListener('mouseup',handleMouseUp)
 
     useEffect(() => {
-      console.log("decal 추가!", decalName);
       
-      if (konvaLayer) {
+      if (konvaLayer && decalName) {
         let image = new Image();
         image.src = decalName;
         let imageObj = new Konva.Image({
@@ -217,7 +224,32 @@ const KonvaCanvas = (props) => {
           image: image,
           width: 100,
           height: 100,
+          draggable : true,
         })
+
+        setAllDecals([...allDecals, imageObj]);
+        
+
+            // 클릭 핸들러 추가
+        imageObj.on('click', (e) => {
+          const metaPressed = e.evt.shiftKey || e.evt.ctrlKey || e.evt.metaKey;
+          const isSelected = selectedNodes.indexOf(imageObj) >= 0;
+        
+          if (!metaPressed && !isSelected) {
+            // 클릭된 데칼을 선택하도록 처리
+            konvaTr.nodes([imageObj]);
+            setSelectedNodes([imageObj]);
+          } else if (metaPressed && isSelected) {
+            // 선택된 데칼을 선택 해제하도록 처리
+            const nodes = selectedNodes.slice();
+            nodes.splice(nodes.indexOf(imageObj), 1);
+            konvaTr.nodes(nodes);
+          } else if (metaPressed && !isSelected) {
+            // 데칼을 선택하도록 처리
+            const nodes = selectedNodes.concat([imageObj]);
+            konvaTr.nodes(nodes);
+          }
+        });
 
     
         konvaLayer.add(imageObj);
@@ -230,6 +262,7 @@ const KonvaCanvas = (props) => {
     <div style={props.style}>
     <div id="container" style={containerStyle} onMouseDown={handleMouseDown}   ></div>
     <button onClick={()=>destroySelected()}>삭제</button>
+    <button onClick={destroyAll}>전체삭제</button>
     <div>{decalName}</div>
     <div>{decalNum}</div>
    </div>
